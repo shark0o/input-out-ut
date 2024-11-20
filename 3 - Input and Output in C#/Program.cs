4d9081ef1a6e0347cb7861b7a4715a2d55d4e1b6
shark0o <shahrokhrahmati8@gmail.com>
2024-11-20 13:30:41 +0200

    Add files via upload

---
+++ 3 - Input and Output in C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3___Input_and_Output_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name");
+            string username = Console.ReadLine();
+            Console.WriteLine("hello," + username); 
+
+        }
+    }
+}
